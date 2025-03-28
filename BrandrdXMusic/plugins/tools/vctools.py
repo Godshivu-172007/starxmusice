35094fbe2a8cab1903a4eee2ea7225affdd26e45
bhushan <97792475+Godshivu-172007@users.noreply.github.com>
2025-03-28 11:59:15 +0530

    Update vctools.py

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -57,16 +57,19 @@ async def brah3(app: app, message: Message):
         print(f"Error: {e}")
 
 
-####
-
 
 @app.on_message(filters.command("math", prefixes="/"))
+
 def calculate_math(client, message):
+    if "/math " not in message.text:
+        message.reply("Error: Please provide a valid mathematical expression after '/math'.")
+        return
+
     expression = message.text.split("/math ", 1)[1]
     try:
         result = eval(expression)
-        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
-    except:
+        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs: {result}"
+    except Exception:
         response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
     message.reply(response)
 
