bcccb0d0a086061c595968783de30f54726cb175
bhushan <97792475+Godshivu-172007@users.noreply.github.com>
2025-03-28 13:45:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://bgkk-spotify.onrender.com/",
+    "",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
